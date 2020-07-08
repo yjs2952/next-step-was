186c81c89e257ced8d016219c8c28686d16df949
yjs2952 <yjs2952@naver.com>
2020-07-08 23:35:46 +0900

    아... 모르겠다

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,12 +3,16 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import db.DataBase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(WebServer.class);
@@ -27,12 +31,22 @@ public class RequestHandler extends Thread {
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             String requestUrl = getRequestUrl(br.readLine());
 
-            printRequestInfo(br);
+            List<HttpRequestUtils.Pair> headers = getHeaders(br);
 
-            if (requestUrl != null && requestUrl.contains("create")) {
+            if (requestUrl != null && requestUrl.contains("/user/create")) {
                 Map<String, String> paramsMap = HttpRequestUtils.parseQueryString(getParams(requestUrl));
                 User user = new User(paramsMap.get("userId"), paramsMap.get("password"), paramsMap.get("name"), paramsMap.get("email"));
                 System.out.println(user.toString());
+                DataBase.addUser(user);
+
+                String line;
+                while ((line = br.readLine()) != null) {
+                    System.out.println(br.readLine());
+
+                    if (line.equals("")) {
+                        break;
+                    }
+                }
             }
 
             DataOutputStream dos = new DataOutputStream(out);
@@ -54,15 +68,17 @@ public class RequestHandler extends Thread {
         return requestUrl.substring(0, index);
     }
 
-    private void printRequestInfo(BufferedReader br) throws IOException {
+    private List<HttpRequestUtils.Pair> getHeaders(BufferedReader br) throws IOException {
+        List<HttpRequestUtils.Pair> headers = new ArrayList<>();
         String line;
         while ((line = br.readLine()) != null) {
             System.out.println(line);
-
+            headers.add(HttpRequestUtils.parseHeader(line));
             if (line.equals("")) {
                 break;
             }
         }
+        return headers;
     }
 
     private String getRequestUrl(String line) {
